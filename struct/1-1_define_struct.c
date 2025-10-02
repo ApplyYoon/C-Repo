f1d5bd4b3d990af971f29a0154b71772a1ab958c
apply <wonji052271@gmail.com>
2025-10-02 20:27:50 +0900

    edit READEME and 1-1_define_struct

--- struct/1-1_define_struct.c
+++ struct/1-1_define_struct.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    struct Student {
+// Define Struct
+struct Student {
         char name[20];
         int age;
         double score;
     };
     // 위와 같이 정의(선언)하면 struct Student라는 새로운 자료형이 생긴다.
     // name, age, score는 "맴버 변수"라고 부른다.
+
+int main() {
+    // 구조체 변수 선언
+    struct Student s;
+
+    // 멤버에 값 넣기
+    s.age = 18;
+    s.score = 95.5;
+
+    // 출력
+    printf("나이: %d, 점수: %.1f\n", s.age, s.score);
+
     return 0;
 }
